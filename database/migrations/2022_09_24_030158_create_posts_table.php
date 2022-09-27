fa60fa20ea0bf23387c95d7201daaf8c4e88bd47
Bahrul Rozak <bahrulrozak13062001@gmail.com>
2022-09-27 09:40:13 +0700

    fresh database

---
+++ database/migrations/2022_09_24_030158_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->string('slug');
+            $table->bigInteger('category_id')->unsigned();
+            $table->text('content');
+            $table->timestamps();
+        });
+
+        //create pivot table post_tag
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->integer('post_id');
+            $table->integer('tag_id');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
